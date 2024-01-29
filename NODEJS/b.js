98c424f83a1c91057e92890062e5489a750bd143
skt <sauravkailastemgire@mitacsc.edu.in>
2024-01-29 22:25:55 +0530

    js

--- NODEJS/b.js
+++ NODEJS/b.js
@@ -2,5 +2,17 @@ const sum = (a,b) => a+b;
 const mul = (a,b) => a*b;
 const g=9.8;
 const pi=3.14;
-
+// exporting 123
+//module.exports=123;
+// Exporting Object
+let obj={
+    name:"saurav",
+    age:22,
+    lastName:"S"
+   , sum:sum,
+   mul:mul,
+   g:g,
+   PI:pi
+};
+module.exports=obj;
 
--- NODEJS/script.js
+++ NODEJS/script.js
@@ -1,6 +1,17 @@
-n=5;
-for(let i=0;i<n;i++)
-{
-console.log("heloo",i);
-}
+// n=5;
+// for(let i=0;i<n;i++)
+// {
+// console.log("heloo",i);
+// }
 
+
+//export using module.exports=123;
+//in from b.js
+    // |
+    // |  
+    // |
+
+const b =require("./b");
+ console.log(b);
+// console.log(b.pi);
+// console.log(b.mul(3,4));
