e79efc68f76f25057a676e71dfee05e149bc42ae
skt <sauravkailastemgire@mitacsc.edu.in>
2024-01-31 17:00:14 +0530

    Insta JSON

--- EJS/index.js
+++ EJS/index.js
@@ -1,11 +1,50 @@
 const express =require("express");
-const app= express();
-const port=8090;
+const app= express();  // expess auto require the ejs no require to require("ejs")
+const port=8091;
+const path=require("path"); //use for set views path
+
+//Start the Server;
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
-})
+});
+
+//set engine as ejs
 app.set("view engine","ejs");
-app.get("/",(req,res)=>
+// set path for views
+app.set("views",path.join(__dirname,"/views"));
+
+// render an Ejs 
+
+app.get("/home",(req,res)=>
 {
-    res.render("home.js");
-})
+    res.render("home.ejs");
+});
+
+// Parse value in ejs
+
+app.get("/dice",(req,res)=>{
+ let  dicee= Math.floor(Math.random()*6)+1;
+    res.render("rolldice",{dicevalue : dicee}); 
+});
+ //
+
+//instagram demo ejs
+
+app.get("/ig/:username",(req,res)=>
+{
+    // const follower=["saurav","gaurav","sargam","suraj"];
+    // let {username}=req.params;
+    const insd=require("./data.json");
+    const {username}=req.params;
+    let data=insd[username];
+    // console.log(data);
+    if(data)
+    {
+        res.render("instagram",{data});
+    }
+    else
+    {
+        res.render("notfound")
+    }
+    
+});
