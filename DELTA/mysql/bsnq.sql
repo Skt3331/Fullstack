8ff8880bfe0092898e031b3123f7630c8e7eff66
skt <sauravkailastemgire@mitacsc.edu.in>
2024-02-04 21:11:21 +0530

    sql with js

---
+++ DELTA/mysql/bsnq.sql
@@ -0,0 +1,13 @@
+create database delta_app;
+use delta_app;
+
+create table temp(
+id Int Primary key);
+commit;
+
+create table user(
+id varchar(50) primary key,
+username varchar(50) unique,
+email varchar(50) unique not null,
+password varchar(50) not null);
+select * from user;
