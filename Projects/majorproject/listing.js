b46f4f1e5dc569740f852f7763908481a4a5006f
skt <sauravkailastemgire@mitacsc.edu.in>
2024-02-19 15:19:40 +0530

    listing

--- Projects/majorproject/listing.js
+++ Projects/majorproject/listing.js
@@ -9,7 +9,10 @@ const listingSchema=new Schema(
             required:true,
         },
         description:String,
-        image:String,
+        image:{
+            type:String,
+            set:(v)=> v==="" ? "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vectors%2Fdefault-vectors&psig=AOvVaw273fULGieqYHAzVB6tGvz0&ust=1708422511005000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCODR4PiPt4QDFQAAAAAdAAAAABAE" :v
+        },
         price :Number,
         location:String,
         country:String,
